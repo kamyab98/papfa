b731ae4d074c060b6c37fa386f5d70171a26831c
Kamyab Zareh <kamyab.zareh@gmail.com>
2023-01-24 16:29:09 +0330

    Bump version: 0.1.11 → 0.1.12

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/kamyab98/papfa',
-    version='0.1.11',
+    version='0.1.12',
     zip_safe=False,
 )
